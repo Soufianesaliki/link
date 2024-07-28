fd9941a444429961c4d9b8a6f30902b92c544355
Soufianesaliki <soufianesaliki1@gmail.com>
2024-07-28 12:14:13 -0700

    souf

--- 0x01-ES6_promise/7-load_balancer.js
+++
@@ -1,7 +0,0 @@
-// Write and export a function named loadBalancer
-// It should accept two arguments chinaDownload (Promise)
-// and USDownload (Promise).
-
-export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]);
-}
